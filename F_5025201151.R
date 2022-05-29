a98a37f98a0f654db88f6170bb87591e70eb1873
Gery Febrian <92217354+GeryFebrian@users.noreply.github.com>
2022-05-29 19:19:31 +0700

    Add files via upload

---
+++ F_5025201151.R
@@ -0,0 +1,114 @@
+# Soal 1
+# 1a
+# Data Sebelum melakukan aktivitas
+x <- c(78, 75, 67, 77, 70, 72, 78, 74, 77)
+
+# Data Setelah melakukan aktivitas
+y <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+
+# Standar Devisiasi selisih pasangan
+sd(x-y)
+
+# 1b
+t.test(x, y, alternative = "greater", var.equal = FALSE)
+
+# 1c
+
+# Soal 2
+install.packages("BSDA")
+library(BSDA)
+
+# 2a
+# Penjelasan di readme
+
+# 2b
+zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,  
+alternative = "greater", mu = 20000,
+conf.level = 0.95) 
+
+# 2c
+# Penjelasan di readme
+
+# Soal 3
+# 3a
+# Penjelasan di readme
+
+
+# 3b 
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
+          mean.y =2.79 , s.y = 1.32, n.y = 27, 
+          alternative = "greater", var.equal = TRUE)
+
+# 3c
+install.packages("mosaic")
+library(mosaic)
+
+plotDist(dist='t', df=2, col="black")
+
+# 3d
+qt(p = 0.05, df = 2, lower.tail=FALSE)
+
+# 3e
+# Penjelasan di readme
+
+# 3f
+# Penjelasan di readme
+
+# Soal 4
+# 4a
+
+nova  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(nova)
+head(nova)
+attach(nova)
+nova$V1 <- as.factor(nova$V1)
+nova$V1 = factor(nova$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(nova$V1)
+
+group1 <- subset(nova, V1=="Kucing Oren")
+group2 <- subset(nova, V1=="Kucing Hitam")
+group3 <- subset(nova, V1=="Kucing Putih")
+
+
+# Soal 5
+# 5a
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+# 5b
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+# 5c
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+# 5d
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+# 5e
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+cld <- as.data.frame.list(tukey.cld$`Glass:Temp_Factor`)
+data_summary$Tukey <- cld$Letters
+print(data_summary)
+
+write.csv("GTL_summary.csv")
